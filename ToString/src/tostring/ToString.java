c1197ece2a1bd11fe4c3d4ebbc05baddbec1c044
unknown <sunzidanasrin>
2017-10-05 11:43:19 +0600

    To String

---
+++ ToString/src/tostring/ToString.java
@@ -0,0 +1,29 @@
+
+package tostring;
+
+import java.util.Date;
+
+
+public class ToString {
+    
+
+    
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+        System.out.println("After using toString:"+now.toString());
+        
+        
+        ////////
+        String x = "10";
+        int y = Integer.parseInt(x);
+        String z = Integer.toString(y);
+        System.out.println("x as String:"+x);
+        System.out.println("y as int:"+y);
+        System.out.println("z as int:"+z);
+    }
+
+    
+    }
+    
+
